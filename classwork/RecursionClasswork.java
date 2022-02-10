0be8cc99b5253252316a148b237c70ab7a6ae219
aahanmehta <amehta30@stuy.edu>
2022-02-10 00:22:08 -0500

    RecursionClasswork added odd10sort, fix long code l8r

--- classwork/RecursionClasswork.java
+++ classwork/RecursionClasswork.java
@@ -4,12 +4,12 @@ public class RecursionClasswork{
     //System.out.println(partalSums(a, 9));
 
     // System.out.println(groupSum6(0, new int[]{5, 6, 2}, 8));
-    System.out.println(groupSum6(0, new int[]{5, 6, 2}, 9));
-    System.out.println(groupSum6(0, new int[]{5, 6, 2}, 7));
+    System.out.println(splitOdd10(new int[]{5, 5,5}));
+    System.out.println(splitOdd10(new int[]{5, 5, 6}));
   }
 
   public static boolean groupSum(int[] nums, int target){
-    return groupSum(nums, target, 0);
+    return groupSum(0, nums, target);
   }
 
   public static boolean groupSum(int start, int[] nums, int target){
@@ -38,6 +38,15 @@ public class RecursionClasswork{
       return(groupNoAdj(start +2, nums, target - nums[start]) || groupNoAdj(start + 1, nums, target));
       }
     }
-  }
 
+  public static boolean splitOdd10(int[] nums){
+    return odd10sort(nums, 0, 0, 0);
+  }
+  public static boolean odd10sort(int[] nums, int start, int oddSum, int tenSum){
+    if(start >= nums.length && (tenSum%10 != 0 || oddSum %2 != 1))return false;
+    if(start >= nums.length && (tenSum%10 == 0 || oddSum %2 == 1))return true;
+    else{
+      return odd10sort(nums, start+1, oddSum + nums[start], tenSum) || odd10sort(nums, start+1, oddSum, tenSum + nums[start]);
+    }
+  }
 }
