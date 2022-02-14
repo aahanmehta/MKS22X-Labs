a2af9f11652b8d94c91c05e97cc83909f7d473e8
aahanmehta <amehta30@stuy.edu>
2022-02-14 10:16:03 -0500

    added toString, addQueen and half of remove

---
+++ 02-queens/Driver.java
@@ -0,0 +1,6 @@
+public class Driver{
+  public static void main(String[] args){
+    QueenBoard board = new QueenBoard(0);
+    System.out.println(board);
+  }
+}
---
+++ 02-queens/QueenBoard.java
@@ -0,0 +1,100 @@
+public class QueenBoard{
+  private int[][] board;
+
+  public QueenBoard(int size){
+    board = new int[size][size];
+    for(int i = 0; i < size; i++){
+      for(int j =0; j < size; j++){
+        board[i][j] = 0;
+      }
+    }
+  }
+  /**
+  *@return The output string formatted as follows:
+  *All numbers that represent queens are replaced with 'Q'
+  *all others are displayed as underscores '_'
+  *There are spaces between each symbol:
+  *_ _ Q _
+  *Q _ _ _
+  *_ _ _ Q
+  *_ Q _ _
+  *excludes the characters up to the comment(*)
+  */
+  public String toString(){
+    String Board = "";
+    for(int i = 0; i < board.length; i++){
+      for (int j = 0; j < board.length; j++){
+        if(board[i][j] == -1)Board += "Q ";
+        else Board += "_ ";
+      }
+      Board += "\n";
+    }
+    return Board;
+  }
+
+  /**
+  *@return true when the queen added correctly, false Otherwise
+  *@postcondition the board is only changed when the function returns true
+  * in which case the queen is added and all it's threatened positions are incremented
+  */
+  private boolean addQueen(int r, int c){
+    if(board[r][c] != 0) return false;
+    board[r][c] == -1;
+    for(int i = 0; i < board.length; i ++){
+      board[i][c] += 1;
+    }
+    for(int j = 0; j < board.length; j ++){
+      board[r][j] += 1;
+    }
+    for(int i = 0; i < board.length - r; i++){
+      for(int j = 0; j < board.length - c; j++){
+        board[i+r][j+c] += 1;
+      }
+    }
+    return true;
+  }
+
+  /**Remove the queen that was added to r,c
+  *@precondition there is a queen at position r,c
+  *@postcondition the board is modified to remove that queen and all it's
+  *threatened positions are decremented
+  */
+  private void removeQueen(int r, int c){
+    if(board[r][c] != -1) return false;
+    board[r][c] == -1;
+    for(int i = 0; i < board.length; i ++){
+      board[i][c] += 1;
+    }
+    for(int j = 0; j < board.length; j ++){
+      board[r][j] += 1;
+    }
+    for(int i = 0; i < board.length - r; i++){
+      for(int j = 0; j < board.length - c; j++){
+        board[i+r][j+c] += 1;
+      }
+    }
+    return true;
+  }
+  }
+
+  // /**Find the first solution configuration possible for this size board. Start by placing
+  // *  the 1st queen in the top left corner, and each new queen in the next ROW. When backtracking
+  // *  move the previous queen to the next valid space. This means everyone will generate the same
+  // *  first solution.
+  // *@postcondition: the board remains in a solved state.
+  // *@return false when the board is not solveable and leaves the board filled with zeros;
+  // *        returns true when the board is solveable, and leaves the board in a solved state
+  // *@throws IllegalStateException when the board starts with any non-zero value (e.g. you solved a 2nd time.)
+  // */
+  // public boolean solve(){
+  //
+  // }
+  //
+  // /**Find all possible solutions to this size board.
+  // *@return the number of solutions found, and leaves the board filled with only 0's
+  // *@throws IllegalStateException when the board starts with any non-zero value (e.g. you ran solve() before this method)
+  // */
+  // public int countSolutions(){
+  //
+  // }
+}
