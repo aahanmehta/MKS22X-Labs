0407f5b5993d0b30718872fd958c8cd323ae4ae1
aahanmehta <amehta30@stuy.edu>
2022-03-06 10:59:19 -0500

    added start+end to gen

--- 03-maze/MazeGenerator.java
+++ 03-maze/MazeGenerator.java
@@ -33,19 +33,42 @@ public class MazeGenerator{
     return dirs;
   }
 
-  public static void generate(char[][]maze, int startrow, int startcol){
+  public static void generat(char[][]maze, int startrow, int startcol){
     if(maze[startrow][startcol] == '#' && !atBorder(maze, startrow, startcol) && lonely(maze, startrow, startcol)){
       maze[startrow][startcol] = ' ';
       int[] direcs = pickDir();
       for(int i =0; i < 4; i++){
-        if(direcs[i] == 1)generate(maze, startrow+1, startcol);
-        if(direcs[i] == 2)generate(maze, startrow-1, startcol);
-        if(direcs[i] == 3)generate(maze, startrow, startcol+1);
-        if(direcs[i] == 4)generate(maze, startrow, startcol-1);
+        if(direcs[i] == 1)generat(maze, startrow+1, startcol);
+        if(direcs[i] == 2)generat(maze, startrow-1, startcol);
+        if(direcs[i] == 3)generat(maze, startrow, startcol+1);
+        if(direcs[i] == 4)generat(maze, startrow, startcol-1);
       }
     }
     else{
       return;
     }
+  }
+
+  public static void generate(char[][]maze, int startrow, int startcol){
+    generat(maze, startrow, startcol);
+    maze[startrow][startcol] = 'S';
+    int dist = 0;
+    for(int i = 1; i < maze.length -1; i++){
+      for(int j = 1; j < maze[0].length -1; j++){
+        if(maze[i][j] == ' '){
+          int len = Math.abs(i-startrow) + Math.abs(j-startcol);
+          if(len > dist)dist = len;
+        }
+      }
+    }
+    for(int i = 1; i < maze.length -1; i++){
+      for(int j = 1; j < maze[0].length -1; j++){
+        if(maze[i][j] == ' '){
+          int len = Math.abs(i-startrow) + Math.abs(j-startcol);
+          if(len == dist)maze[i][j] = 'E';
+        }
+      }
+    }
+
   }
 }
