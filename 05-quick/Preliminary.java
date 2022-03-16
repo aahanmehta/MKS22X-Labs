651dbd32744c7316aba2e1098dd9c1ec9e1c9f2a
aahanmehta <amehta30@stuy.edu>
2022-03-16 09:34:13 -0400

    push

--- 05-quick/Preliminary.java
+++ 05-quick/Preliminary.java
@@ -1,4 +1,22 @@
+import java.util.Arrays;
 public class Preliminary{
+  public static void main(String[] args) {
+    int [] d1 = new int[] {4,3,2,1,0};
+    int [] d2 = new int[] {0};
+    int [] d3 = new int[] {1,1,1,1,1};
+    int [] d4 = new int[] {993,994,995,4,3,2,1,0,997,998,999};
+    int [] d5 = new int[] {1};
+    int [] d6 = new int[] {997,998,999,4,3,2,1,0};
+    int [] d7 = new int[] {999,5,999,5,1,4,2,5,5,999,99};
+    int[][] data = new int[][] {d1,d2,d3,d4,d5,d6,d7};
+    for(int i = 0; i < data.length; i++){
+      System.out.println("Original: "+Arrays.toString(data[i]));
+      int pivot = partition( data[i] , 0, data[i].length-1);
+      System.out.println("Pivot value: "+data[i][pivot]+ ", Pivot index: "+pivot);
+      System.out.println("Modified: "+Arrays.toString(data[i]));
+      System.out.println();
+    }
+  }
   /*Modify the array such that:
   *1. A random index from start to end inclusive is chosen, the
   * corresponding element is designated the pivot element.
@@ -10,12 +28,16 @@ public class Preliminary{
   *@return the index of the final position of the pivot element.
   */
   public static int partition ( int [] data, int start, int end){
-    System.out.println(toString(data));
+    //System.out.println(toString(data));
+    if(data.length<1)return -1;
+    if(data.length==1)return 0;
     int index = (int)(Math.random()*(end-start+1) + start);
-    int pivot = data[start];
+    int pivot = data[index];
     int x = 0;
+    boolean alt = true;
     index = start;
     swap(data, start, index);
+
     for(int i = start; i <= end - x; i++){
       if(data[i] < pivot){
         swap(data, i, index);
@@ -24,12 +46,25 @@ public class Preliminary{
       else if(data[i] > pivot){
         swap(data, i, end - x);
         i--;
-        x--;
+        x++;
       }
+      // else if(data[i] == pivot){
+      //   if(alt){
+      //     swap(data, i, end -x);
+      //     i--;
+      //     x++;
+      //     alt = false;
+      //   }
+      //   else{
+      //     swap(data, i, index);
+      //     index++;
+      //     alt = true;
+      //   }
+      // }
     }
 
-    System.out.println(pivot);
-    System.out.println(toString(data));
+    // System.out.println(pivot);
+    // System.out.println(toString(data));
     return index;
   }
 
@@ -39,14 +74,6 @@ public class Preliminary{
     data[y] = swap;
   }
 
-  public static void main(String[] args) {
-    int[] t1 = {1,7,5,2,3,4};
-    int[] t2 = {999,999,999,4,3,2,1,0,999,999,999};
-    //System.out.println(toString(t1));
-    System.out.println(partition(t1, 0, 5));
-    //System.out.println(toString(t2));
-    //System.out.println(partition(t2, 0, 9));
-  }
   public static String toString(int[] ary){
     String ans = "[";
     for(int c:ary){
