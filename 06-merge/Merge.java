cd4dfac545cb8b7cc5a79c529955ddb1d25988e1
aahanmehta <amehta30@stuy.edu>
2022-03-21 18:21:46 -0400

    think i finsished ez mode mergesort, tested it with raymonds and someone elses testers and iz cool so yea.

--- 06-merge/Merge.java
+++ 06-merge/Merge.java
@@ -2,25 +2,72 @@ import java.util.Arrays;
 public class Merge{
 
   public static void main(String[] args) {
-    int[] t1 = {1,2,54,5,7};
-    int[] t2 = {5,1,2,3,5,6};
-    int[] yea = merge(t1,t2);
-    System.out.println(Arrays.toString(yea));
+    int[] t1 = {1,2,5,7,54};
+    int[] t2 = {1,2,3,5,6};
+    int[] t3 = {53,5,56,2,4,36,43,1,6,2};
+    // int[] yea = merge(t1,t2);
+    System.out.println(Arrays.toString(t3));
+    mergesort(t3);
+    System.out.println(Arrays.toString(t3));
+
+    // System.out.println(Arrays.toString(yea));
   }
 
   public static int[] merge(int[] left, int[] right){
     int[] merged = new int[left.length + right.length];
     int lCt = 0;
     int rCt = 0;
-    System.out.println(merged.length);
+    int i = 0;
+    while(lCt<left.length && rCt<right.length){
+      if(left[lCt] < right[rCt]){
+        merged[i] = left[lCt];
+        i++;lCt++;
+      }
+      else{
+        merged[i] = right[rCt];
+        i++;rCt++;
+      }
+      //System.out.println(Arrays.toString(merged));
+    }
+    for(int j = rCt; j < right.length; j++){
+      merged[i] = right[j];
+      i++;
+    }
+    for(int j = lCt; j < left.length; j++){
+      merged[i] = left[j];
+      i++;
+    }
+
+
+    //System.out.println(merged.length);
     return merged;
   }
   public static void mergesort(int[] data){
-
+    int[] temp = mergesortH(data);
+    for(int i = 0; i < temp.length; i++){
+      data[i] = temp[i];
+    }
   }
 
   public static int[] mergesortH(int[] data){
+    if(data.length > 1){
+      int halfway = data.length/2;
+      int[] left = new int[halfway];
+      int[] right = new int[data.length-halfway];
+      copy(left, data, 0, halfway);
+      copy(right, data, halfway, data.length);
+      mergesort(left);
+      mergesort(right);
+      data = merge(left, right);
+    }
     return data;
+  }
+
+  private static void copy(int[] copy, int[] og, int start, int end){
+    for(int i = start; i < end; i++){
+      copy[i-start] = og[i];
+    }
+
   }
 
 }
