45f404948ecd14f283c2c1075bebf09408581b97
aahanmehta <amehta30@stuy.edu>
2022-03-21 10:17:01 -0400

    start

---
+++ 06-merge/Merge.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+public class Merge{
+
+  public static void main(String[] args) {
+    int[] t1 = {1,2,54,5,7};
+    int[] t2 = {5,1,2,3,5,6};
+    int[] yea = merge(t1,t2);
+    System.out.println(Arrays.toString(yea));
+  }
+
+  public static int[] merge(int[] left, int[] right){
+    int[] merged = new int[left.length + right.length];
+    int lCt = 0;
+    int rCt = 0;
+    System.out.println(merged.length);
+    return merged;
+  }
+  public static void mergesort(int[] data){
+
+  }
+
+  public static int[] mergesortH(int[] data){
+    return data;
+  }
+
+}
