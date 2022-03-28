682945262fa3949ecd602707cc4e1fde2197fb52
aahanmehta <amehta30@stuy.edu>
2022-03-28 09:51:41 -0400

    starting constructor

---
+++ 07-mydeque/MyDeque.java
@@ -0,0 +1,34 @@
+public class MyDeque<E>{
+  private E[] data;
+  private int size, start, end;
+
+  public MyDeque(){
+    @SuppressWarnings("unchecked")
+    E[] d = (E[])new Object[10];
+    data = d;
+    size = 0;
+  }
+  public MyDeque(int initialCapacity){
+    @SuppressWarnings("unchecked")
+    E[] d = (E[])new Object[initialCapacity];
+    data = d;
+    size = 0;
+  }
+  public int size(){
+    if(end < start){
+      return data.length - (start-end);
+    }
+    else{
+      return end - start;
+    }
+  }
+  public String toString(){ }
+  public void addFirst(E element){ }
+  public void addLast(E element){ }
+  public E removeFirst(){ }
+  public E removeLast(){ }
+  public E getFirst(){ }
+  public E getLast(){ }
+}
+
+}
