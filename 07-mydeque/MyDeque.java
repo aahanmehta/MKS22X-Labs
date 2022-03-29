461391b785a678889fb8f8373ad4b7825b2c5b7f
aahanmehta <amehta30@stuy.edu>
2022-03-28 20:47:02 -0400

    kinda works, need to figure out kinks

--- 07-mydeque/MyDeque.java
+++ 07-mydeque/MyDeque.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class MyDeque<E>{
   private E[] data;
   private int size, start, end;
@@ -6,13 +7,13 @@ public class MyDeque<E>{
     @SuppressWarnings("unchecked")
     E[] d = (E[])new Object[10];
     data = d;
-    size = 0;
+    size = 0; start = 0; end = 0;
   }
   public MyDeque(int initialCapacity){
     @SuppressWarnings("unchecked")
     E[] d = (E[])new Object[initialCapacity];
     data = d;
-    size = 0;
+    size = 0; start = 0; end = 0;
   }
 
   public int size(){
@@ -21,7 +22,7 @@ public class MyDeque<E>{
 
   public String toString(){
     String ans = "[";
-    if(size == 0; return ans+"]");
+    if(size == 0) return ans+"]";
 
     if(start <= end){
       for(int i = start; i<=end; i++){
@@ -35,7 +36,7 @@ public class MyDeque<E>{
     }
 
     if(start > end){
-      for(int i = start; i < data.length; i++)}{
+      for(int i = start; i < data.length; i++){
         ans += data[i] + ", ";
       }
       for(int i =0; i <= end; i++){
@@ -52,38 +53,90 @@ public class MyDeque<E>{
 
   public void addFirst(E element){
     if(size == data.length){
-      
+      expand();
     }
-    if(size == 0){
-      start = 0;
-      data[start] = element;
-      end = 0;
-    }
-
+    if(size == 0)data[start] = element;
     else{
-      if(start >0){
-        start--;
-        data[start] = element;
-      }
-      else{
-        start == data.length-1;
-        data[]
-      }
+      start--;
+      if(start < 0)start = data.length-1;
+      data[start] = element;
     }
+    size++;
+  }
+
+  public void addLast(E element){
+    if(size == data.length){
+      expand();
+    }
+    if(size == 0)data[end] = element;
+    else{
+      end++;
+      if(end >= data.length)end = 0;
+      data[end] = element;
+    }
+    size++;
+  }
+
+  private void expand(){
+    @SuppressWarnings("unchecked")
+    E[] newThing = (E[])new Object[data.length*2];
+    int copIn = start;
+    for(int i = 0; i < size; i++){
+      if(copIn == data.length)copIn = 0;
+      newThing[i] = data[copIn];
+      copIn++;
+    }
+    data = newThing;
+    start = 0;
+    end = size-1;
 
   }
 
-  public void addLast(E element){ }
 
-  public E removeFirst(){ }
 
-  public E removeLast(){ }
+  public E removeFirst(){
+    if(size == 0)throw new NoSuchElementException("size 0");
+    E removed = data[start];
+    start++;
+    if(start >= data.length)start = 0;
+    size--;
+    return removed;
+
+  }
+
+  public E removeLast(){
+    if(size == 0)throw new NoSuchElementException("size 0");
+    E removed = data[end];
+    end--;
+    if(end < 0)end = data.length-1;
+    size--;
+    return removed;
+
+  }
 
   public E getFirst(){
+    if(size == 0)throw new NoSuchElementException("size 0");
     return data[start];
   }
 
   public E getLast(){
+    if(size == 0)throw new NoSuchElementException("size 0");
     return data[end];
+  }
+
+  public static void main(String[] args) {
+    MyDeque a = new MyDeque<Integer>(2);
+    System.out.println(a.toString());
+    System.out.println(a.size());
+    a.addFirst(1);
+    a.addFirst(1342);
+    System.out.println(a.size());
+    a.addFirst(13);
+    a.addLast(99);
+    System.out.println(a.size());
+    a.addLast(142);
+    System.out.println(a.size());
+    System.out.println(a.toString());
+    // System.out.println(a.size());
   }
 }
